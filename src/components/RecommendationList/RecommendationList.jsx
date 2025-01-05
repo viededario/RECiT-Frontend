34e80edebec6011167aa87c78ca8bb7aa982c572
viededario <darioocceus@gmail.com>
2025-01-05 11:49:08 -0500

    creating branch to test merge last pushes

--- src/components/RecommendationList/RecommendationList.jsx
+++ src/components/RecommendationList/RecommendationList.jsx
@@ -1,5 +1,6 @@
 import { React, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
+import './RecommendationList.css'
 
 const RecommendationList = ({ recommendations, handleLikeRecommendation, handleDislikeRecommendation }) => {
   const handleLikeClick = (event, recommendationId) => {
